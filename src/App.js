81c1c0ccc18478674fd1ebb9e5d880b9f27cce7b
iharris06 <ian.harris4@gmail.com>
2020-06-20 13:54:09 -0700

    Add bootstrap navbar.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Link, Route, Switch} from 'react-router-dom';
+import {Link, Route} from 'react-router-dom';
 import Projects from './components/Projects';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-        </header> */}
-        <body>
-        <nav class="navbar">
-            <div class="brand-title">Ian Harris</div>
-            <a href="#" class="toggle-button">
-              <span class="bar"></span>
-              <span class="bar"></span>
-              <span class="bar"></span>
-            </a>
-            <div class="nav-links"> 
-              <ul>  
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-              </ul>
-            </div>
-          </nav>
-        </body>
+        <Navbar>
+            <Navbar.Brand className="brand-title">Ian Harris</Navbar.Brand>            
+            <Nav className="nav-links">  
+                <Nav.Link><Link to="/">Home</Link></Nav.Link>
+                <Nav.Link><Link to="/projects">Projects</Link></Nav.Link>
+                <Nav.Link><Link to="/about">About</Link></Nav.Link>
+                <Nav.Link><Link to="/contact">Contact</Link></Nav.Link>
+            </Nav>
+        </Navbar>
+        
         <Route exact= {true} path="/" component={Home}/>
         <Route path="/projects" component={Projects}/>
         <Route path="/about" component={About}/>
