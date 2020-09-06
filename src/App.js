870d14fc94c888ffd7fe18f95f7c3405adcc5d45
iharris06 <ian.harris4@gmail.com>
2020-09-05 19:41:19 -0700

    fix navbar and add styling to home page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Link, Route} from 'react-router-dom';
+import {Link,Route} from 'react-router-dom';
 import Projects from './components/Projects';
 import Home from './components/Home';
 import About from './components/About';
@@ -13,20 +13,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar collapseOnSelect expand="md" bg="light">
-            <Navbar.Brand>
-              <Link to="/" className="brand-title" style={{ textDecoration: 'none', color:'rgba(0, 0, 0, 0.9)' }}>Ian Harris</Link>
-            </Navbar.Brand>    
-            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>        
-            <Navbar.Collapse className="mr-auto">
-            <Nav className="nav-links">  
-                <Nav.Link eventKey="1"><Link to="/">Home</Link></Nav.Link>
-                <Nav.Link eventKey="2"><Link to="/projects">Projects</Link></Nav.Link>
-                <Nav.Link eventKey="3"><Link to="/about">About</Link></Nav.Link>
-                <Nav.Link eventKey="4"><Link to="/contact">Contact</Link></Nav.Link>
-            </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <nav className="navbar navbar-expand-lg navbar-dark" expand="md">
+            <Link to="/" className="navbar-brand brand-title" style={{ textDecoration: 'none', color:"white" }}>Ian Harris</Link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <Link className="nav-item nav-link link-text-dark" to="/">Home</Link>
+                <Link className="nav-item nav-link link-text-dark" to="/projects">Projects</Link>
+                <Link className="nav-item nav-link link-text-dark" to="/about">About</Link>
+                <Link className="nav-item nav-link link-text-dark" to="/contact">Contact</Link>
+            </div>
+            </div>
+        </nav>
         
         <Route exact= {true} path="/" component={Home}/>
         <Route path="/projects" component={Projects}/>
